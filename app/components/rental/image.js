903e15c829a75449d91b156c045eb26ed926932f
Andrea Freeman <afreeman5566@gmail.com>
2022-03-18 15:57:42 -0500

    added image test

--- app/components/rental/image.js
+++ app/components/rental/image.js
@@ -3,7 +3,7 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class RentalImageComponent extends Component {
-
+  
     @tracked isLarge = false;  // @tracked tells emebr to track the changes of the specified instance variable
 
     @action toggleSize() { //@action tells ember we want to use this method for our template
