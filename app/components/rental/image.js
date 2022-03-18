e86c6bc381948d78e63bb5361f1b74939ecf9ec3
Andrea Freeman <afreeman5566@gmail.com>
2022-03-18 15:31:20 -0500

    replaced image text with buttons that use state

--- app/components/rental/image.js
+++ app/components/rental/image.js
@@ -6,7 +6,7 @@ export default class RentalImageComponent extends Component {
 
     @tracked isLarge = false;  // @tracked tells emebr to track the changes of the specified instance variable
 
-    @action toggleSize() { //
+    @action toggleSize() { //@action tells ember we want to use this method for our template
       this.isLarge = !this.isLarge;
     }
 }
