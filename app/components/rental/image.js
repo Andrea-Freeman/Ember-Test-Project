4c3dc6c9007d9a0147a81e3ed7455d71868aefbc
Andrea Freeman <afreeman5566@gmail.com>
2022-03-18 15:12:56 -0500

    refactored image.js state code

--- app/components/rental/image.js
+++ app/components/rental/image.js
@@ -2,10 +2,7 @@ import Component from '@glimmer/component';
 
 export default class RentalImageComponent extends Component {
 
-    constructor(...args) {
-        super(...args);
-        this.isLarge = false;
-      }
+   isLarge = false
 }
 
 //older way of calling state here 
