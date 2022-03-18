bd6b121eb4fdb53d849c0f71a3c942395f348d31
Andrea Freeman <afreeman5566@gmail.com>
2022-03-18 15:25:16 -0500

    added {{tracked}} - tells ember to track changes to a specific instance variable

--- app/components/rental/image.js
+++ app/components/rental/image.js
@@ -1,9 +1,15 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking'; 
+import { action } from '@ember/object';
 
 export default class RentalImageComponent extends Component {
 
-   isLarge = false
+    @tracked isLarge = false;  // @tracked tells emebr to track the changes of the specified instance variable
+
+    @action toggleSize() { //
+      this.isLarge = !this.isLarge;
+    }
 }
 
-//older way of calling state here 
+
 //this file holds the state
